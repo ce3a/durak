e059c672f84ce5a2cf3229a597e3e6e71e5a77dc
Sergej Sawazki <ce3a@gmx.de>
2018-12-30 22:39:43 +0100

    main: Test Deck::draw()

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,32 @@ auto main(int argc, char *argv[]) -> int
     (void)argc;
     (void)argv;
 
+    int i;
     Deck deck{};
 
-    auto print_deck = [&deck]() {
-      for (const auto& card: deck)
-          std::cout << card << std::endl;
+    auto print_deck = [&]() {
+        i = 1;
+        for (const auto& card: deck)
+            std::cout << "* " << i++ << ": " << card << std::endl;
+    };
+
+    auto print_size = [&]() {
+        std::cout << "number of cards: " << deck.size() << std::endl;
     };
 
     print_deck();
+    print_size();
 
     deck.shuffle();
 
+    std::cout << "---" << std::endl;
+
+    i = 1;
+    while (!deck.empty()) {
+        std::cout << "- " << i++ << ": " << deck.draw() << std::endl;
+    }
+
+    print_size();
     std::cout << "---" << std::endl;
     print_deck();
 
